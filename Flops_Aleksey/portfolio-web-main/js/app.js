f94f5bbd09fecbd157ede24b29dbd762687234c7
Aleksey2023dev <124687283+Aleksey2023dev@users.noreply.github.com>
2023-03-23 15:25:57 +0400

    Add files via upload

---
+++ Flops_Aleksey/portfolio-web-main/js/app.js
@@ -0,0 +1,53 @@
+const header = document.querySelector("header");
+
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll("counter span");
+const progress_bars = document.querySelectorAll("skills svg cirle");
+
+window.addEventListener("scroll", () => {
+	skillsCounter();
+})
+
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+
+function stickyNavbar(){
+	header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
+
+/* --------------- Reveal Animation --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+function hasReached(el) {
+	let topPosition = el.getBoundingClientRect().top;
+
+	if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+		return false;
+}
+
+function scillsCounter() {
+		//	console.log("You've  reached the skills section");
+		hasReached(first_scills);
+}
